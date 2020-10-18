3c5884594ed9f1861644e9b7f503d9b8041af28f
Julie-Ha <69715947+Julie-Ha@users.noreply.github.com>
2020-10-18 09:37:44 +0200

    Updates

---
+++ precache-manifest.0bae437393782e626145ddefe7c92328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc40e9f67ab30e3e8ea059d0995b537",
+    "url": "/random-menu/index.html"
+  },
+  {
+    "revision": "e12e5a8106e3ba58ead9",
+    "url": "/random-menu/static/css/main.07f1f0ae.chunk.css"
+  },
+  {
+    "revision": "b6689ddff7aecb255081",
+    "url": "/random-menu/static/js/2.2b149c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-menu/static/js/2.2b149c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12e5a8106e3ba58ead9",
+    "url": "/random-menu/static/js/main.139b6424.chunk.js"
+  },
+  {
+    "revision": "62c4bc62ea1e951334f5",
+    "url": "/random-menu/static/js/runtime-main.e7056333.js"
+  }
+]);
